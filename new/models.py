8da07b52424bc8d2b1ace37b05a1b0ee35e3665e
Nikita <ns45134513@gmail.com>
2026-02-25 16:39:00 +0530

    Updated settings for Render

--- new/models.py
+++ new/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
 class Profile(models.Model):
     name = models.CharField(max_length=100)
     title = models.CharField(max_length=200)
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2918zq8ady9*-n6m*v3xa7akca+x%lacc$qde&ck@6yb7fd8mm'
+# SECRET_KEY = 'django-insecure-2918zq8ady9*-n6m*v3xa7akca+x%lacc$qde&ck@6yb7fd8mm'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = os.getenv("DEBUG", "False") == "True"
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
